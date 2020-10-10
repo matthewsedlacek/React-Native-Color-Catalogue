4a7a2706b846e1a226eeae364126eb6748692d94
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-10-10 10:57:00 -0700

    adds device info Platform, Height, Width, and adds rendering for Android

--- App.js
+++ App.js
@@ -4,18 +4,35 @@ import {
   View,
   ActivityIndicator,
   ProgressViewIOS,
+  ProgressBarAndroid,
   Button,
+  Alert,
+  Dimensions,
+  Platform,
 } from "react-native";
+
+const { height, width } = Dimensions.get("window");
 
 export default function App() {
   const onButtonPress = () => {
-    console.log(`${new Date().toLocaleTimeString()} button press`);
+    Alert.alert(`${new Date().toLocaleTimeString()} button press`);
   };
   return (
     <View style={{ padding: 50 }}>
-      <ProgressViewIOS progress={0.5} />
+      {Platform.OS === "ios" && <ProgressViewIOS progress={0.5} />}
+      {Platform.OS === "android" && (
+        <ProgressBarAndroid
+          styleAttr="Horizontal"
+          indeterminate={false}
+          color="blue"
+          progress={0.5}
+        />
+      )}
       <ActivityIndicator size="large" color="#61DBF" />
       <Button title="click me" onPress={onButtonPress} />
+      <Text>Platform: {Platform.OS}</Text>
+      <Text>Height: {height}</Text>
+      <Text>Width: {width}</Text>
     </View>
   );
 }
