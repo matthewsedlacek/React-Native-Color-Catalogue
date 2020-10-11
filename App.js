2404169b45e9f7fc5d6dd1e92686d616d68c5e46
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-10-10 18:17:04 -0700

    creates example.txt folder for earlier example and changes App.js to show box with color names and formatting

--- App.js
+++ App.js
@@ -1,38 +1,29 @@
-import * as React from "react";
-import {
-  Text,
-  View,
-  ActivityIndicator,
-  ProgressViewIOS,
-  ProgressBarAndroid,
-  Button,
-  Alert,
-  Dimensions,
-  Platform,
-} from "react-native";
-
-const { height, width } = Dimensions.get("window");
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const onButtonPress = () => {
-    Alert.alert(`${new Date().toLocaleTimeString()} button press`);
-  };
   return (
-    <View style={{ padding: 50 }}>
-      {Platform.OS === "ios" && <ProgressViewIOS progress={0.5} />}
-      {Platform.OS === "android" && (
-        <ProgressBarAndroid
-          styleAttr="Horizontal"
-          indeterminate={false}
-          color="blue"
-          progress={0.5}
-        />
-      )}
-      <ActivityIndicator size="large" color="#61DBF" />
-      <Button title="click me" onPress={onButtonPress} />
-      <Text>Platform: {Platform.OS}</Text>
-      <Text>Height: {height}</Text>
-      <Text>Width: {width}</Text>
+    <View style={styles.page}>
+      <Text style={styles.text}>red</Text>
+      <Text style={[styles.text, styles.selectedText]}>green</Text>
+      <Text style={styles.text}>blue</Text>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  page: {
+    marginTop: 40,
+    backgroundColor: "#DDD",
+  },
+  text: {
+    fontSize: 22,
+    color: "red",
+    backgroundColor: "yellow",
+    margin: 10,
+    padding: 5,
+  },
+  selectedText: {
+    backgroundColor: "red",
+    color: "yellow",
+  },
+});
