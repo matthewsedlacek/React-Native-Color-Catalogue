d32dd466d1db1396d6f7eaeda402b2eae7cc463d
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-10-12 10:58:48 -0700

    adds Color Form component

--- App.js
+++ App.js
@@ -2,23 +2,27 @@ import React, { useState } from "react";
 import { StyleSheet, FlatList } from "react-native";
 import ColorButton from "./components/ColorButton.js";
 import defaultColors from "./data/defaultColors.json";
+import ColorForm from "./components/ColorForm.js";
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
   return (
-    <FlatList
-      style={[styles.container, { backgroundColor }]}
-      data={defaultColors}
-      renderItem={({ item }) => {
-        return (
-          <ColorButton
-            key={item.id}
-            backgroundColor={item.color}
-            onPress={setBackgroundColor}
-          />
-        );
-      }}
-    />
+    <>
+      <ColorForm />
+      <FlatList
+        style={[styles.container, { backgroundColor }]}
+        data={defaultColors}
+        renderItem={({ item }) => {
+          return (
+            <ColorButton
+              key={item.id}
+              backgroundColor={item.color}
+              onPress={setBackgroundColor}
+            />
+          );
+        }}
+      />
+    </>
   );
 }
 
